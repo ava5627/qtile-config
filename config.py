ad105a30fd042f139187d9d38e6e30d3330ede8e
ava5627 <avasharris1@gmail.com>
2024-03-05 19:44:46 -0600

    monitor fix

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ from libqtile.config import Click, Drag, EzKey, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.log_utils import logger
 
-from group_config import go_to_group, group_keys, groups_list, group_screen
+from group_config import go_to_group, group_keys, groups_list, group_screen, get_num_monitors
 
 import json
 
@@ -531,33 +531,6 @@ def parse_nightscout(data):
         delta_s = ""
 
     return f"{arrow} {glucose} {delta_s}mg/dL "
-
-
-def get_num_monitors():
-    num_monitors = 0
-    try:
-        from Xlib import display as xdisplay
-        display = xdisplay.Display()
-        screen = display.screen()
-        resources = screen.root.xrandr_get_screen_resources()
-
-        for output in resources.outputs:
-            monitor = display.xrandr_get_output_info(output, resources.config_timestamp)
-            preferred = False
-            if hasattr(monitor, "preferred"):
-                preferred = monitor.preferred
-            elif hasattr(monitor, "num_preferred"):
-                preferred = monitor.num_preferred
-            if preferred:
-                num_monitors += 1
-    except ImportError:
-        logger.error("Xlib is not installed")
-    except Exception as e:
-        # always setup at least one monitor
-        logger.error(f"Exception while getting num monitors: {e}")
-        return 1
-    else:
-        return num_monitors
 
 
 num_monitors = get_num_monitors()
--- group_config.py
+++ group_config.py
@@ -1,7 +1,33 @@
 from libqtile.config import Group, Match
 from libqtile.lazy import lazy
+from libqtile.log_utils import logger
 
-from config import get_num_monitors
+
+def get_num_monitors():
+    num_monitors = 0
+    try:
+        from Xlib import display as xdisplay
+        display = xdisplay.Display()
+        screen = display.screen()
+        resources = screen.root.xrandr_get_screen_resources()
+
+        for output in resources.outputs:
+            monitor = display.xrandr_get_output_info(output, resources.config_timestamp)
+            preferred = False
+            if hasattr(monitor, "preferred"):
+                preferred = monitor.preferred
+            elif hasattr(monitor, "num_preferred"):
+                preferred = monitor.num_preferred
+            if preferred:
+                num_monitors += 1
+    except ImportError:
+        logger.error("Xlib is not installed")
+    except Exception as e:
+        # always setup at least one monitor
+        logger.error(f"Exception while getting num monitors: {e}")
+        return 1
+    else:
+        return num_monitors
 
 
 def _go_to_group(name):
