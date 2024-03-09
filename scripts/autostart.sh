b9237629f3fc61ac315bf1cf56a1e34777af964b
ava5627 <avasharris1@gmail.com>
2024-03-08 19:11:19 -0600

    nix autostart

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -3,23 +3,22 @@ source /etc/os-release
 if [ "$ID" != "nixos" ]; then
     nm-applet &
     xfce4-power-manager &
-    blueberry-tray &
     picom --config $HOME/.config/qtile/scripts/picom.conf &
     /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
     dunst &
     playerctld &
-    solaar -w hide &
-    # starting user applications at boot time
-    discord &
-    steam -silent &
-    openrgb -p Off &
-    kdeconnect-indicator &
-    morgen --hidden &
-    insync start &
     flameshot &
-    firefox &
 fi
 
 #starting utility applications at boot time
+openrgb -p Off &
+blueberry-tray &
 feh --no-fehbg --bg-scale $HOME/Pictures/'Saved Pictures'/Wallpapers/camp_fire.jpg &
 copyq &
+solaar -w hide &
+discord &
+steam -silent &
+kdeconnect-indicator &
+morgen --hidden &
+insync start --no-daemon &
+firefox &
